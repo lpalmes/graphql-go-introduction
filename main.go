998614e8b356e90a038823fff234bfc9ec9ee6e1
lpalmes <lpalmes@gmail.com>
2017-09-25 21:41:16 -0300

    Added viewer example

--- main.go
+++ main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"time"
 
 	"io/ioutil"
 
 	"fmt"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/neelance/graphql-go"
 	"github.com/neelance/graphql-go/relay"
 )
+
+var PublicKey = []byte("secret")
+
+type user struct {
+	ID       string
+	Name     string
+	Mail     string
+	Password string
+}
+
+type LoginInput struct {
+	Mail     string
+	Password string
+}
+
+var users = []user{
+	{
+		ID:       "1",
+		Name:     "Example User",
+		Mail:     "example@mail.com",
+		Password: "example",
+	},
+	{
+		ID:       "2",
+		Name:     "Test User",
+		Mail:     "test@mail.com",
+		Password: "test",
+	},
+}
+
+type viewerResolver struct {
+	User user
+}
 
 // This will be use by our handler at /graphql
 var schema *graphql.Schema
@@ -35,6 +72,53 @@ func init() {
 	}
 }
 
+//Helper function
+func checkToken(jwtToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return PublicKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if token.Valid {
+		return token, nil
+	}
+
+	return token, nil
+}
+
+func generateToken(user user) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"exp": time.Now().Add(time.Hour * time.Duration(24)).Unix(), //This token will live for 24 hours
+		"iat": time.Now().Unix(),
+		"sub": user.ID,
+	})
+	tokenString, err := token.SignedString(PublicKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		token := r.Header.Get("Authorization")
+		jwt, err := checkToken(token)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		next.ServeHTTP(w, r.WithContext(context.WithValue(ctx, "jwt", jwt)))
+	})
+}
+
 func main() {
 	// We will start a small server that reads our "graphiql.html" file and
 	// responds with it, so we are able to have our own graphiql
@@ -48,7 +132,7 @@ func main() {
 
 	// This is where our graphql server is handled, we declare "/graphql" as the route
 	// where all our graphql requests will be directed to
-	http.Handle("/graphql", &relay.Handler{Schema: schema})
+	http.Handle("/graphql", auth(&relay.Handler{Schema: schema}))
 
 	// We start the server by using ListenAndServe and we log if we have any error, hope not!
 	fmt.Println("Listening at http://localhost:8080")
@@ -62,4 +146,70 @@ type Resolver struct{}
 // Hello function resolves to hello: String! in the Query object in our schema.graphql
 func (r *Resolver) Hello() string {
 	return "world"
+}
+
+func (r *Resolver) Login(args *struct {
+	Input *LoginInput
+}) (string, error) {
+	for _, user := range users {
+		if user.Mail == args.Input.Mail {
+			if user.Password == args.Input.Password {
+				token, err := generateToken(user)
+				if err != nil {
+					return "", err
+				}
+				return token, err
+			} else {
+				return "", errors.New("password is incorrect")
+			}
+		}
+	}
+
+	return "", errors.New("User not found")
+}
+
+func (r *Resolver) Viewer(ctx context.Context, args *struct {
+	Token *string
+}) (*viewerResolver, error) {
+
+	token := ctx.Value("jwt").(*jwt.Token)
+	if token == nil && args.Token == nil {
+		return nil, errors.New("There needs to be a token in the Authorization header or viewer input")
+	}
+
+	if token == nil && args.Token != nil {
+		viewerToken, err := checkToken(*args.Token)
+		if err != nil {
+			return nil, err
+		}
+
+		token = viewerToken
+	}
+
+	claims, _ := token.Claims.(jwt.MapClaims)
+	id := claims["sub"].(string)
+
+	var user user
+
+	for _, u := range users {
+		if id == string(u.ID) {
+			user = u
+		}
+	}
+
+	return &viewerResolver{
+		User: user,
+	}, nil
+}
+
+func (v *viewerResolver) ID() graphql.ID {
+	return graphql.ID(v.User.ID)
+}
+
+func (v *viewerResolver) Name() string {
+	return v.User.Name
+}
+
+func (v *viewerResolver) Mail() string {
+	return v.User.Mail
 }
